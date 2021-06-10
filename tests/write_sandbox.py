1c598580f47d0536f6d80b63e7fb333502020396
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-06-10 17:47:03 +0200

    Update write_sandbox.py

--- tests/write_sandbox.py
+++ tests/write_sandbox.py
@@ -1,5 +1,7 @@
+print("oh ok give me a sec")
 import pywikibot
 site = pywikibot.Site()
 page = pywikibot.Page(site, "User:VukkyBot/sandbox")
-page.text = input("New sandbox text: ")
+page.text = input("what do ya wanna write: ")
+print("okay sure saving that, whatever you say")
 page.save("Sandbox test")
