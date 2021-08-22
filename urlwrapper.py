3f6b114e549402e43b166efb282c8f4da6d4c277
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-08-22 15:10:07 +0200

    Update urlwrapper.py

--- urlwrapper.py
+++ urlwrapper.py
@@ -7,25 +7,29 @@ headers = {
     'User-Agent': 'VukkyBotURLWrapper/1.0 - User:Vukky',
 }
 
+dump_file = "/public/dumps/public/enwiki/latest/" + random.choice([x for x in os.listdir("/public/dumps/public/enwiki/latest") if "-pages-articles" in x and x.endswith(".bz2") and not "multistream" in x and os.path.isfile(os.path.join("/public/dumps/public/enwiki/latest", x))])
+dump_parsed = xmlreader.XmlDump(dump_file).parse()
+
 def report_problem(page):
     problem = False
     pageSplit = page.text.splitlines()
     for line in pageSplit:
         if not_edge_case(line) is True:
             problem = True
+        if incompatible(line) is True:
+            problem = False
             break
     if problem is True:
         return True
     else:
         return False
 
-dump_file = "/public/dumps/public/enwiki/latest/" + random.choice([x for x in os.listdir("/public/dumps/public/enwiki/latest") if "-pages-articles" in x and x.endswith(".bz2") and not "multistream" in x and os.path.isfile(os.path.join("/public/dumps/public/enwiki/latest", x))])
-dump_parsed = xmlreader.XmlDump(dump_file).parse()
-
 def treat_page(page, save):
     pageSplit = page.text.splitlines()
     changed = False
     for index, line in enumerate(pageSplit):
+        if incompatible(line) is True:
+            return
         if not_edge_case(line) is True:
             print(line.replace("http", "{{URL|http", 1) + "}}")
             pageSplit[index] = line.replace("http", "{{URL|http", 1) + "}}"
@@ -41,6 +45,12 @@ def treat_page(page, save):
 
 def not_edge_case(line):
     if "website" in line and not "*" in line and not "{{" in line and not "[" in line and not "<!--" in line and not "<ref>" in line and not "</ref>" in line and "http" in line:
+        return True
+    else:
+        return False
+
+def incompatible(line):
+    if "{{infobox television" in line.lower():
         return True
     else:
         return False
