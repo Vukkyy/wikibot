d668a9408470d6d7a0dd18d13b2c6b774b8a2e3f
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-08-22 14:45:54 +0200

    Update urlwrapper.py

--- urlwrapper.py
+++ urlwrapper.py
@@ -25,7 +25,7 @@ def treat_page(page, save):
     pageSplit = page.text.splitlines()
     changed = False
     for index, line in enumerate(pageSplit):
-        if "website" in line and not "{{" in line and not "[" in line and "http" in line:
+        if "website" in line and not "{{" in line and not "[" in line and not "<!--" in line and "http" in line:
             print(line.replace("http", "{{URL|http", 1) + "}}")
             pageSplit[index] = line.replace("http", "{{URL|http", 1) + "}}"
             changed = True
